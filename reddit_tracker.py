b09c447d7f7845043e37e7c962f04ef0f1e5aab4
mobiuscreek <vyronc@bgs.ac.uk>
2021-02-14 15:52:06 +0000

    First attempt at collecting all posts"

--- reddit_tracker.py
+++ reddit_tracker.py
@@ -10,17 +10,34 @@ def conv_time(posix_time):
     time = datetime.datetime.utcfromtimestamp(posix_time).strftime('%Y-%m-%d')
     return time
 
-def collect_subs(sub_name):
+def collect_comments(sub_list):
     ''' Collect posts from subreddits '''
-    sub_list = []
+    comments = []
 
-    for submission in reddit.subreddit(sub_name).hot(limit=1):
-        logging.info(submission.title)
-        submission.comments.replace_more()
-        sub_list = submission.comments.list()
-    return sub_list
+    for sub_url in sub_list:
+        sub = reddit.submission(url=sub_url)
+        sub.comments.replace_more(limit=None)
+        for comment_id in sub.comments.list():
+            comments.append(comment_id)
 
-def collect_historical_subs(sub_name, query):
+    return comments
+
+def collect_historical_subs(sub_name, query_str, start_time, end_time):
+    ''' Return subs that match query in text of top submissions based on dates'''
+    historical_subs = []
+
+    for submission in reddit.subreddit(sub_name).top(limit=100):
+        sub_time = conv_time(submission.created_utc)
+        if query_str in submission.selftext and start_time <= sub_time <= end_time:
+            historical_subs.append(submission.url)
+        else:
+            continue
+
+    return historical_subs
+
+
+def collect_hot_subs(sub_name, query_str):
+    ''' Return subs that match query in text of hot submissions '''
     historical_subs = []
     for submission in reddit.subreddit(sub_name).search(query):
         if conv_time(submission.created_utc) == '2021-01-27':
