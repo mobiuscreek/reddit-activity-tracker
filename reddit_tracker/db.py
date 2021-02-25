4d69c085f776503a0738679484ef0b03474f6f0a
mobiuscreek <vyronc@bgs.ac.uk>
2021-02-25 19:27:21 +0000

    Add example query and fix write to api call

--- reddit_tracker/db.py
+++ reddit_tracker/db.py
@@ -3,19 +3,21 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 def db_connect():
     ''' Connect to influxdb'''
-    client = InfluxDBClient.from_config_file("praw.ini")
+    client = InfluxDBClient.from_config_file("praw.ini") # Configuration of influxdb lives in praw.ini too!
     return client
 
 def db_write(client, bucket, org, data):
     write_api = client.write_api(write_options=SYNCHRONOUS)
-    for data_point in data:
-        write_api.write(bucket, org, data_point)
+    write_api.write(bucket, org, data)
 
-def db_read(client, bucket):
+def check_exists(client, bucket, created_utc):
     ''' Example query '''
     db_values = []
-    tables = client.query_api().query('from(bucket:"reddit_tracker") |> range(start: -10m) |> filter(fn: (r) => r.subreddit == "wallstreetbets")')
-    for table in tables:
+
+    response = client.query_api().query(f'from(bucket:"{bucket}")'
+                                        f'|> range (start: 2021-01-01T00:00:00Z, stop: 2021-02-21T00:00:00Z)'
+                                        f'|> filter(fn: (r) => r._time == {created_utc})') 
+    for table in response:
         for record in table.records:
             db_values.append(record.values)
     return db_values
